8874618ceb1a480dfe6d140e9edbd4146997d030
Lhcfl <lhcfl@outlook.com>
2025-08-17 23:09:10 +0800

    👩‍💻 dev: use experimental container api

--- src/utils/gen-description.ts
+++ src/utils/gen-description.ts
@@ -5,22 +5,38 @@ import { SITE_DESCRIPTION } from "@/consts";
 
 type Blog = CollectionEntry<"blog">;
 
+let container: any = null;
+async function getContainer(): ReturnType<typeof experimental_AstroContainer.create> {
+  return container ||= await experimental_AstroContainer.create();
+}
+
 function generateDescriptionFromContent(html: string): string {
   const moreIndex = html.indexOf("<!-- more");
   const excerpt = html.slice(0, moreIndex == -1 ? undefined : moreIndex);
   return excerpt;
 }
 
+async function getRenderedContent(blog: Blog) {
+  const container = await getContainer();
+  const { Content } = await render(blog); // Ensure the blog is rendered to access the body
+  return await container.renderToString(Content);
+}
+
 export const generateDescription = async (blog: Blog) => {
   if (blog.data.description) return blog.data.description;
-  const { Content } = await render(blog); // Ensure the blog is rendered to access the body
-  if (!blog.rendered) {
-    console.warn(
-      `Blog post ${blog.id} (${blog.filePath}) does not have rendered content. Please ensure it is properly rendered.`
-    );
+
+  const rendered = await getRenderedContent(blog).catch(err => {
+    console.error(err);
     return SITE_DESCRIPTION;
-  }
-  const { html: rendered } = blog.rendered;
+  });
+
+  // if (!blog.rendered) {
+  //   console.warn(
+  //     `Blog post ${blog.id} (${blog.filePath}) does not have rendered content. Please ensure it is properly rendered.`
+  //   );
+  //   return SITE_DESCRIPTION;
+  // }
+  // const { html: rendered } = blog.rendered;
 
   const html = blog.data.summary || generateDescriptionFromContent(rendered);
 
